7b69e3abf65879f80bc3675186c7214b6970d032
Ashutosh Kumar <ashutosh.kumar@druva.local>
2019-09-10 14:29:27 +0530

    Updated Rest api with mux router

--- REST/main.go
+++ REST/main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 //Article ... struct definition
 type Article struct {
+	Id      string `json:"id"`
 	Title   string `json:"title"`
 	Desc    string `json:"desc"`
 	Content string `json:"content"`
@@ -26,18 +31,47 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 func handleRequests() {
 
-	http.HandleFunc("/articles", returnAllArticles)
-	http.HandleFunc("/", homePage)
-	log.Fatal(http.ListenAndServe(":10000", nil))
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+	myRouter.HandleFunc("/articles", returnAllArticles)
+	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/addArticle", createNewArticle).Methods("POST")
+	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
 
 func returnAllArticles(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit : returnAllArticles")
-
-	articles := []Article{
-		Article{Title: "ZNMD", Desc: "Zindagi Na Milegi Dobara", Content: "Best Movie Ever"},
-		Article{Title: "CCOB", Desc: "Curious Case of Benjamin Button", Content: "Best Movie Ever 2"},
-	}
-
+	articles := returnArticles()
 	json.NewEncoder(w).Encode(articles)
+}
+
+func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	key, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		fmt.Fprintf(w, "error in conversion ")
+	}
+	articles := returnArticles()
+	//fmt.Fprintf(w, "key : "+key)
+	json.NewEncoder(w).Encode(articles[key])
+
+}
+
+func createNewArticle(w http.ResponseWriter, r *http.Request) {
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var article Article
+	json.Unmarshal(reqBody, &article)
+	articles := returnArticles()
+	articles = append(articles, article)
+	json.NewEncoder(w).Encode(articles)
+}
+
+func returnArticles() []Article {
+	articles := []Article{
+		Article{Id: "1", Title: "ZNMD", Desc: "Zindagi Na Milegi Dobara", Content: "Best Movie Ever"},
+		Article{Id: "2", Title: "CCOB", Desc: "Curious Case of Benjamin Button", Content: "Best Movie Ever 2"},
+	}
+	return articles
 }
